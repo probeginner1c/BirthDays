d0cde3918f8628d045f0b98e7453960b79e2dc5a
probeginner1c <probeginner1c@gmail.com>
2019-09-01 08:47:36 +0000

    Refactored to BirthDays class

---
+++ src/Dates/BirthDates.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Prospero\Dates;
+
+
+class BirthDates
+{
+    private $birthDate;
+    private $todayDate;
+
+    public  function  __construct($birthDate)
+    {
+        $this->birthDate = $birthDate;
+        $this->todayDate = date("Y-m-d");
+    }
+
+    public function findTotalDays() {
+        $birthTime = strtotime($this->birthDate);
+        $todayTime = strtotime($this->todayDate);
+
+        $totalsecs = $todayTime - $birthTime;
+        $totalDays = floor($totalsecs / 3600 / 24);
+
+        return $totalDays;
+    }
+}
